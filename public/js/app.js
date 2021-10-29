d3a67058209e0cd8b0237275d6e02ec1b4dab55f
Migue <miguel.galdamez@hotmail.com>
2021-10-29 05:17:58 +0000

    en el server

--- public/js/app.js
+++ public/js/app.js
@@ -6650,7 +6650,7 @@ __webpack_require__.r(__webpack_exports__);
 
 var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0___default()(function(i){return i[1]});
 // Module
-___CSS_LOADER_EXPORT___.push([module.id, "\n.mostrar{\r\n    display:list-item;\r\n    opacity: 1;\r\n    background: rgba(44,38,75,0.849);\n}\r\n", ""]);
+___CSS_LOADER_EXPORT___.push([module.id, "\n.mostrar{\n    display:list-item;\n    opacity: 1;\n    background: rgba(44,38,75,0.849);\n}\n", ""]);
 // Exports
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
 
